cc677164e9fee02cc9c538b7f2cb8621cb161072
WillY <ykywilly@gmail.com>
2026-01-31 18:59:48 +0800

    project restructured with monorepo approach

--- src/main/java/com/vpm/vocalpitchmonitor/repositories/AudiotrackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vpm.vocalpitchmonitor.repositories;
-
-import com.vpm.vocalpitchmonitor.entities.Audiotrack;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AudiotrackRepository extends JpaRepository<Audiotrack, Integer> {
-}
--- src/main/java/com/vpm/vocalpitchmonitor/repositories/LyricsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vpm.vocalpitchmonitor.repositories;
-
-import com.vpm.vocalpitchmonitor.entities.SyncedLyrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LyricsRepository extends JpaRepository<SyncedLyrics, Integer> {
-}
