340075b8d4ca821bd7ce775e95fab2baab30f276
WillY <ykywilly@gmail.com>
2026-02-06 21:41:08 +0800

    api updates

--- server/src/main/java/com/vpm/vocalpitchmonitor/controllers/StreamController.java
+++ server/src/main/java/com/vpm/vocalpitchmonitor/controllers/StreamController.java
@@ -8,11 +8,13 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/api")
 public class StreamController {
 
     public StreamService streamService;
